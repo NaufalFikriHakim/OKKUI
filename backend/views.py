eb0ea92659e91c8bf2dccc7aacfeaad3deb499b5
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-03-03 20:29:08 +0700

    finish API for Pembicara

--- backend/views.py
+++ backend/views.py
@@ -31,6 +31,7 @@ class SponsorCreateReadAPIView(APIView):
         return Response(serializer.data)
     
     def post(self, request, acara_id):
+        request.data['acara'] = acara_id
         serializer = SponsorSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -38,6 +39,12 @@ class SponsorCreateReadAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class SponsorUpdateDeleteAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Sponsor.objects.get(pk=pk)
+        except Sponsor.DoesNotExist:
+            return None
+
     def put(self, request, pk):
         sponsor = self.get_object(pk)
         serializer = SponsorSerializer(sponsor, data=request.data)
@@ -49,4 +56,39 @@ class SponsorUpdateDeleteAPIView(APIView):
     def delete(self, request, pk):
         sponsor = self.get_object(pk)
         sponsor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PembicaraCreateReadAPIView(APIView):
+    def get(self, request, acara_id):
+        pembicaras = Pembicara.objects.filter(acara=acara_id)
+        serializer = PembicaraSerializer(pembicaras, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, acara_id):
+        request.data['acara'] = acara_id  # Set the acara field based on the URL parameter
+        serializer = PembicaraSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class PembicaraUpdateDeleteAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Pembicara.objects.get(pk=pk)
+        except Pembicara.DoesNotExist:
+            return None
+
+    def put(self, request, pk):
+        pembicara = self.get_object(pk)
+        serializer = PembicaraSerializer(pembicara, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        pembicara = self.get_object(pk)
+        pembicara.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
